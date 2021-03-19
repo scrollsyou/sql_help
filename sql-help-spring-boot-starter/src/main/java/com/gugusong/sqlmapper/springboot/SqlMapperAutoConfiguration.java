5fdc252aae7c20d97f3d7be4c15f82fdb42d127c
you <you@DESKTOP-PHQLC3H>
2021-03-19 13:42:43 +0800

    增加springboot配置

---
+++ sql-help-spring-boot-starter/src/main/java/com/gugusong/sqlmapper/springboot/SqlMapperAutoConfiguration.java
@@ -0,0 +1,113 @@
+package com.gugusong.sqlmapper.springboot;
+
+import java.sql.SQLException;
+
+import javax.annotation.Resource;
+import javax.sql.DataSource;
+import javax.xml.ws.FaultAction;
+
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+
+import com.gugusong.sqlmapper.PageHelp;
+import com.gugusong.sqlmapper.Session;
+import com.gugusong.sqlmapper.SessionFactory;
+import com.gugusong.sqlmapper.common.util.SnowFlake;
+import com.gugusong.sqlmapper.config.GlogalConfig;
+import com.gugusong.sqlmapper.db.ColumnTypeMapping;
+import com.gugusong.sqlmapper.db.PageHelpImpl;
+import com.gugusong.sqlmapper.db.SessionFactoryImpl;
+import com.gugusong.sqlmapper.db.mysql.ColumnTypeMappingImpl;
+import com.gugusong.sqlmapper.strategy.ImplicitNamingStrategy;
+import com.gugusong.sqlmapper.strategy.impl.DefaultJDBCImplicitNamingStrategyImpl;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * 自动配置sqlMapper
+ * @author yousongshu
+ *
+ */
+@Slf4j
+@Configuration
+@EnableConfigurationProperties(SqlMapperProperties.class)
+@ConditionalOnBean(DataSource.class)
+public class SqlMapperAutoConfiguration implements DisposableBean {
+
+	@Resource
+	private SqlMapperProperties properties;
+	
+	/**
+	 * 雪花随机数生成器
+	 * 可自定义编写
+	 * @return
+	 */
+	@ConditionalOnMissingBean(SnowFlake.class)
+	@Bean
+	public SnowFlake snowFlake() {
+		return new SnowFlake(properties.getDatacenterId(), properties.getMachineId());
+	}
+	
+	/**
+	 * 命名转换策略
+	 * @return
+	 */
+	@ConditionalOnMissingBean(ImplicitNamingStrategy.class)
+	@Bean
+	public ImplicitNamingStrategy implicitNamingStrategy() {
+		return new DefaultJDBCImplicitNamingStrategyImpl();
+	}
+	
+	/**
+	 * java类中属性类型与数据库字段类型映射
+	 * @return
+	 */
+	@ConditionalOnMissingBean(ColumnTypeMapping.class)
+	@Bean
+	public ColumnTypeMapping columnTypeMapping() {
+		return new ColumnTypeMappingImpl();
+	}
+	
+	/**
+	 * 分页处理器
+	 * @return
+	 */
+	@ConditionalOnMissingBean(PageHelp.class)
+	@Bean
+	public PageHelp pageHelp() {
+		return new PageHelpImpl();
+	}
+	
+	@ConditionalOnMissingBean(GlogalConfig.class)
+	@Bean
+	public GlogalConfig glogalConfig(DataSource dataSource, SnowFlake snowFlake, ImplicitNamingStrategy implicitNamingStrategy,
+			ColumnTypeMapping columnTypeMapping, PageHelp pageHelp) {
+		return new GlogalConfig(dataSource, snowFlake, implicitNamingStrategy, columnTypeMapping, pageHelp);
+	}
+	
+	@ConditionalOnMissingBean(SessionFactory.class)
+	@Bean
+	public SessionFactory sessionFactory(GlogalConfig config) {
+		return new SessionFactoryImpl(config);
+	}
+	
+	@Bean
+	@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+	public Session session(SessionFactory factory) throws SQLException {
+		return factory.openSession();
+	}
+	
+	@Override
+	public void destroy() throws Exception {
+		log.info("sql-help destroy...");
+
+	}
+
+}
